94a5c91e95a70820bd88e07984bbb328cbd0f338
Spencer Hurley <hurle156@umn.edu>
2020-03-20 15:27:59 -0500

    Easy portion-Date only filter

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,23 @@
 // from data.js
 var tableData = data;
-
+var tbody = d3.select("tbody");
+// console.log(tableData);
 // YOUR CODE HERE!
+var filter = d3.select("#filter-btn");
+
+filter.on("click", function(){
+    // console.log("we have a click")
+    var input = d3.select(".form-control");
+    // console.log("we have input");
+    var date = input.property("value");
+    // console.log(date);
+    var filteredData = tableData.filter(d => d.datetime == date);
+    // console.log(filteredData);
+    filteredData.forEach(function(table){
+        var row = tbody.append("tr")
+        Object.entries(table).forEach(function([key, value]){
+            var cell = row.append("td");
+            cell.text(value);
+        })
+    })
+})
