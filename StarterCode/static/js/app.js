46d312a3b43e78fcba0835f278e53b2a930d9dc7
Spencer Hurley <hurle156@umn.edu>
2020-03-20 17:17:38 -0500

    added different filter options and fixed just adding to the last table problem.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -4,14 +4,28 @@ var tbody = d3.select("tbody");
 // console.log(tableData);
 // YOUR CODE HERE!
 var filter = d3.select("#filter-btn");
-
+tableData.forEach(function(table){
+    var row = tbody.append("tr")
+    Object.entries(table).forEach(function([key, value]){
+        var cell = row.append("td");
+        cell.text(value);
+    })
+})
 filter.on("click", function(){
+    tbody.html("");
     // console.log("we have a click")
-    var input = d3.select(".form-control");
+    var inputDate = d3.select("#datetime");
+    var inputState = d3.select("#state");
+    var inputCity = d3.select("#city");
+    var inputCountry = d3.select("#country");
     // console.log("we have input");
-    var date = input.property("value");
+    var date = inputDate.property("value");
+    var state = inputState.property("value");
+    var city = inputCity.property("value");
+    var country = inputCountry.property("value");
     // console.log(date);
-    var filteredData = tableData.filter(d => d.datetime == date);
+    var filteredData = tableData.filter(d => d.datetime == date || 
+        d.state == state|| d.city == city || d.country == country);
     // console.log(filteredData);
     filteredData.forEach(function(table){
         var row = tbody.append("tr")
